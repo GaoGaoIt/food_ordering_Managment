4c12cace3ee007712c5d7a2424d231d593b0ea6f
sam <samchan952@gmail.com>
2023-11-03 11:42:57 +0800

    update homepage design

--- components/ProductsComponent/index.vue
+++ components/ProductsComponent/index.vue
@@ -2,46 +2,54 @@
     <div class="tw-grid-cols-12 tw-gap-4 tw-grid">
         <div class="tw-grid tw-col-span-12 xl:tw-col-span-3 md:tw-col-span-4" v-for='(product, index) in  productsList'>
 
-            <q-card class="tw-m-2">
-                <q-img :src="`${config.public.imageUrl}/${product.image_url}`" :ratio="16 / 9">
-                    <div class="absolute-top-right text-subtitle2 tw-rounded-md">
-                        Rm {{ product.Products_price }}
-                    </div>
-                </q-img>
+            <q-card class="tw-m-2 tw-p-2 tw-rounded-lg">
+                <div class="tw-rounded-lg">
+                    <q-img :src="`${config.public.imageUrl}/${product.image_url}`" :ratio="16 / 9" />
+                </div>
 
-                <q-card-section>
-                    <div class="tw-flex">
-                        <span class="tw-text-xl md:tw-text-2xl tw-font-poppins tw-font-bold">
-                            {{ product.Products_name }}
-                        </span>
-                    </div>
-                    <div class="text-subtitle2"></div>
-                </q-card-section>
+                <div class="tw-bg-gray-400 infoContainer tw-mt-4  tw-h-[160px]">
+                    <q-card-section class="infoTitle">
+                        <div class="tw-flex tw-justify-between tw-items-center ">
+                            <span class="tw-text-lg md:tw-text-lg tw-font-poppins tw-font-bold">
+                                {{ product.Products_name }}
+                            </span>
+                            <span class=" tw-flex tw-text-lg md:tw-text-lg">Rm {{ product.Products_price }}</span>
+                        </div>
+                        <div class="tw-text-lg md:tw-text-lg ">
+                            By Jony Restaurant
+                        </div>
+                        <div>
+                            <span class="tw-text-blue-900 tw-text-md tw-font-semibold">{{
+                                $dayjs(product.Product_start_date).locale('en').format('h.mm a') }} - {{
+                                $dayjs(product.Product_end_date).locale('en').format('h.mm a') }}
+                            </span>
+                        </div>
+                    </q-card-section>
+                    <q-card-actions class="cardAction tw-items-center tw-mt-4">
+                        <div class="tw-flex tw-gap-2">
+                            <q-btn dense unelevated round>
+                                <q-icon name="favorite" color="red" size="md"></q-icon>
+                            </q-btn>
+                            <q-btn dense unelevated round>
+                                <q-icon name="shopping_cart" color="black" size="md"></q-icon>
+                            </q-btn>
+                        </div>
+                        <div>
+                            <q-btn unelevated color="blue" v-if="!authStore.getUser()" class="tw-p-4 tw-w-full tw-w-22">
+                                <span class="tw-mx-auto" @click="tologin">Login</span>
+                            </q-btn>
+                            <q-btn unelevated dense color="blue" v-else>
+                                <span class="tw-text-md ">Checkout</span>
+                            </q-btn>
+                        </div>
+                    </q-card-actions>
+                </div>
 
-                <q-card-section class="tw-flex tw-justify-between tw-items-center">
-                    <div class="tw-flex tw-gap-2">
-                        <q-btn dense unelevated round>
-                            <q-icon name="favorite" color="red" size="md"></q-icon>
-                        </q-btn>
-                        <q-btn dense unelevated round>
-                            <q-icon name="shopping_cart" color="black" size="md"></q-icon>
-                        </q-btn>
-                    </div>
-                    <div>
-                        <q-btn unelevated  color="blue" v-if="!authStore.getUser()" class="tw-p-4 tw-w-full tw-w-22">
-                            <span class="tw-mx-auto" to="/login" >Login</span>
-                        </q-btn>
-                        <q-btn unelevated dense color="blue" v-else>
-                            <span class="tw-text-md ">Checkout</span>
-                        </q-btn>
-
-                    </div>
-                </q-card-section>
             </q-card>
         </div>
     </div>
 </template>
-<style src="./ProductsComponent.scss"></style>
+<style lang="scss" src="./ProductsComponent.scss"></style>
 <script setup lang="ts">
 import { useAuthStore } from '~/stores/AuthStore';
 import { ProductsCard } from '~/utilities/types/ProductsCard';
@@ -59,6 +67,10 @@ const props = withDefaults(
         productsList: [],
     }
 );
+const router = useRouter();
+const tologin = () => {
+    router.push('/login');
+}
 
 onMounted(() => {
 
